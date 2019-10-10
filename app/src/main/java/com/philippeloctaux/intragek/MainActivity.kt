3644dc91528f103e559fbc617c52c5dea8ed93f5
Philippe Loctaux <p@philippeloctaux.com>
2019-10-11 00:48:27 +0200

    extracting text from editText and printing out to console

--- app/src/main/java/com/philippeloctaux/intragek/MainActivity.kt
+++ app/src/main/java/com/philippeloctaux/intragek/MainActivity.kt
@@ -2,11 +2,20 @@ package com.philippeloctaux.intragek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when user taps on Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val login = editText.text.toString()
+        println(login)
     }
 }
