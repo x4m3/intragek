0edf85b28145470eb4c15baa56fe211a44179fb1
Philippe Loctaux <p@philippeloctaux.com>
2019-10-15 00:26:49 +0200

    added autologin link input

--- app/src/main/java/com/philippeloctaux/intragek/MainActivity.kt
+++ app/src/main/java/com/philippeloctaux/intragek/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import com.philippeloctaux.intragek.hideKeyboard
 
 const val STUDENT = "com.philippeloctaux.intragek.STUDENT"
 
